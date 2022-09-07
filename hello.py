07c03cca4a27d6887f231801812fe9c16d57cd73
Alexandre Leroux <alex@sherpa.one>
2022-09-07 22:21:56 +0200

    up

--- hello.py
+++ hello.py
@@ -1,59 +1,25 @@
 from flask import Flask
-from markupsafe import escape
+from flask import url_for
 
 app = Flask(__name__)
 
-@app.route("/<name>")
-def hello(name):
-    '''
-    Affiche dynamiquement un contenu en fonction
-    de la variable <name> renseignée dans l'url.
-    Tester l'URL http://localhost:5000/world
-    '''
-    return f"Hello, {escape(name)}!"
+@app.route('/')
+def index():
+    return 'index'
+
+@app.route('/login')
+def login():
+    return 'login'
 
 @app.route('/user/<username>')
-def show_user_profile(username):
-    '''
-    Affiche dynamiquement un contenu en fonction
-    de la variable <username> renseignée dans l'url.
-    Tester l'URL http://localhost:5000/user/john-doe
-    '''
-    # show the user profile for that user
-    return f'User {escape(username)}'
+def profile(username):
+    return f'{username}\'s profile'
 
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    '''
-    Affiche dynamiquement un contenu en fonction
-    de la variable <post_id> renseignée dans l'url
-    et convertie sous forme d'entier
-    Tester l'URL http://localhost:5000/posts/777
-    '''
-    # show the post with the given id, the id is an integer
-    return f'Post {post_id}'
-
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    # show the subpath after /path/
-    return f'Subpath {escape(subpath)}'
-
-
-@app.route('/projects/')
-def projects():
-    '''
-    L'URL http://localhost:5000/projects est automatiquement redirigée
-    vers l'adresse canonique http://localhost:5000/projects/ (avec un slash à la fin)
-    '''
-    return 'The project page'
-
-@app.route('/about')
-def about():
-    '''
-    L'URL http://localhost:5000/about/ (avec un slash à la fin) conduit à une erreur 404
-    car l'adresse canonique n'en contient pas
-    '''
-    return 'The about page'
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
 
 #Lancer l'application :
 #flask --app hello run
