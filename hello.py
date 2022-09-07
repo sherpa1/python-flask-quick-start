5cde929c5b796e7b678a5bb3181fa61321012374
Alexandre Leroux <alex@sherpa.one>
2022-09-07 17:01:41 +0200

    templates

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ from flask import Flask
 from markupsafe import escape
 from flask import url_for
 from flask import request
-
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -93,6 +93,11 @@ def login():
         return do_the_login()
     else:
         return show_the_login_form()
+    
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
 
 #permet d'afficher directement les données dans la console, au lancement de l'application (utile pour des tests)
 with app.test_request_context():
