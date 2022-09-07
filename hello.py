d82a995b0b6ef3df3a91f5203fe620fe8a046336
Alexandre Leroux <alex@sherpa.one>
2022-09-07 22:29:37 +0200

    up

--- hello.py
+++ hello.py
@@ -1,25 +1,13 @@
 from flask import Flask
-from flask import url_for
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'index'
 
-@app.route('/login')
-def login():
-    return 'login'
-
-@app.route('/user/<username>')
-def profile(username):
-    return f'{username}\'s profile'
-
-with app.test_request_context():
-    print(url_for('index'))
-    print(url_for('login'))
-    print(url_for('login', next='/'))
-    print(url_for('profile', username='John Doe'))
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
 
 #Lancer l'application :
 #flask --app hello run
