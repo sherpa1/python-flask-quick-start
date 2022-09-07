123b2b8a827086caaf2033a23c65527aa191918c
Alexandre Leroux <alex@sherpa.one>
2022-09-07 16:07:38 +0200

    escape

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/<name>")
+def hello_world(name):
+    #pour éviter l'injection de code malveillant en exécutant un url de ce type : http://localhost:5000/<script>alert("bad")</script>
+    return f"<p>Hello, {escape(name)}</p>"
