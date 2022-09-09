efdb45df755df3b8f27144047a9e1ee90a1f440e
Alexandre Leroux <alex@sherpa.one>
2022-09-09 14:36:38 +0200

    add templates + deal login error

--- hello.py
+++ hello.py
@@ -1,13 +1,39 @@
 from flask import Flask
+from flask import request
 from flask import render_template
+from flask import redirect
+from flask import url_for
 
 app = Flask(__name__)
 
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+def valid_login(username,password):
+    if not username or not password:
+        return False
+    elif username == "john@doe.com" and password=="Azerty":
+        return True
+    else:
+        return False
+
+def log_the_user_in(username):
+    return f"<p>Welcome {username}</p>"
+
+@app.route('/')
+def redirect_to_login():
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():    
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
 
 #Lancer l'application :
 #flask --app hello run
@@ -15,4 +41,4 @@ def hello(name=None):
 #Lancer l'application avec le mode debug :
 #flask --app hello --debug run
 
-#Consulter l'application http://localhost:5000
+#Consulter l'application http://localhost:5000/login
