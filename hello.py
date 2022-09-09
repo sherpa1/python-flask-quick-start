a1ef7f0f5d08e08dcbc3a13b516a1f1eed9e92c5
Alexandre Leroux <alex@sherpa.one>
2022-09-09 14:42:37 +0200

    deal 401 and 404 erros with and without template

--- hello.py
+++ hello.py
@@ -3,42 +3,15 @@ from flask import request
 from flask import render_template
 from flask import redirect
 from flask import url_for
+from flask import abort
 
 app = Flask(__name__)
 
 
-def valid_login(username,password):
-    if not username or not password:
-        return False
-    elif username == "john@doe.com" and password=="Azerty":
-        return True
-    else:
-        return False
+@app.route('/unauthorized')
+def render_unauthorized_error():
+    abort(401)
 
-def log_the_user_in(username):
-    return f"<p>Welcome {username}</p>"
-
-@app.route('/')
-def redirect_to_login():
-    return redirect(url_for('login'))
-
-@app.route('/login', methods=['POST', 'GET'])
-def login():    
-    error = None
-    if request.method == 'POST':
-        if valid_login(request.form['username'],
-                       request.form['password']):
-            return log_the_user_in(request.form['username'])
-        else:
-            error = 'Invalid username/password'
-    # the code below is executed if the request method
-    # was GET or the credentials were invalid
-    return render_template('login.html', error=error)
-
-#Lancer l'application :
-#flask --app hello run
-
-#Lancer l'application avec le mode debug :
-#flask --app hello --debug run
-
-#Consulter l'application http://localhost:5000/login
+@app.route('/404')
+def render_404_error():
+    return render_template('404.html')
