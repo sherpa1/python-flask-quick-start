1bcc959e1af708015ff8f45edee184d77707eba9
Alexandre Leroux <alex@sherpa.one>
2022-09-09 15:40:01 +0200

    wip

--- hello.py
+++ hello.py
@@ -2,23 +2,44 @@ from http.client import responses
 from flask import Flask
 from flask import render_template
 from flask import make_response
+from flask import abort
 
 app = Flask(__name__)
 
+users = [
+    {"id":1,"firstname":"John","lastname":"Doe"},
+    {"id":2,"firstname":"James","lastname":"White"},
+]
 
-# @app.errorhandler(404)
-#"""
-#ce code gère les erreurs 404
-#"""
-# def not_found_error(error):
-#     return render_template('404.html'), 404
+def get_all():
+    return users
 
-@app.errorhandler(404)
-def not_found_error_with_headers(error):
-    """
-    ce code gère les erreurs 404 et ajoute un header http personnalisé
-    """
-    response=make_response(render_template('404.html'),404)
-    response.headers["X-Something"]="This a custom header value"
-    return response
+def get_one(id=None):
+    found=None
+    if not id:
+        return None
+    else:
+        for user in users:
+            if user.get('id')==id:
+                found = user
+                break
+            
+    return found
 
+@app.get("/users/<int:id>")
+def read_one(id=None):
+    user = get_one(id)
+    
+    if not user:
+        return render_template('user_not_found.html',id=id),404
+    else:
+        return {
+            "id": user.get("id"),
+            "firstname": user.get("firstname"),
+            "lastname": user.get("firstname"),
+        }
+
+@app.get("/users")
+def read_all():
+    users = get_all()
+    return users
